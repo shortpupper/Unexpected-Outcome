27fe18039c8b27a52bc36fa016885375b14c5d9c
lole <loganboganfarfignogan@gmail.com>
2023-04-23 09:53:01 -0500

    Setting up the base

---
+++ Code/Entity.py
@@ -0,0 +1,8 @@
+
+
+class Entity:
+    def __init__(self, NameID, Name) -> None:
+        self.NameID = NameID
+        self.Name = Name
+    
+    def 
---
+++ Code/attribute.py
@@ -0,0 +1,5 @@
+
+
+class Attribute:
+    def __init__(self) -> None:
+        pass
